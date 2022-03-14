5b11d3cc9defdeb3adfa7b17ffa0736ff7609073
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2022-03-14 18:09:30 +0600

    Add files via upload

---
+++ src/main/java/com/example/postcoreapi/controller/PostController.java
@@ -0,0 +1,55 @@
+package com.example.postcoreapi.controller;
+
+import com.example.postcoreapi.model.PostModel;
+import com.example.postcoreapi.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+
+    @GetMapping("/check")
+    public ResponseEntity<String> check(){
+        return new ResponseEntity<>("post-core-api is working", HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createPost(@Valid @RequestBody PostModel postModel) {
+        postService.createPost(postModel);
+        return new ResponseEntity<>("Successfully created", HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public List<PostModel> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{postID}")
+    public PostModel getPostById(@PathVariable String postID) {
+        return postService.getPostById(postID);
+    }
+
+    @PutMapping("/{postID}")
+    public ResponseEntity<String> updatePostById(@PathVariable String postID,
+                                                     @Valid @RequestBody PostModel postModel) {
+        postService.updatePostById(postID, postModel);
+        return new ResponseEntity<>("Successfully updated", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{postID}")
+    public ResponseEntity<String> deletePostById(@PathVariable String postID) {
+        postService.deletePostById(postID);
+        return new ResponseEntity<>("Successfully deleted", HttpStatus.OK);
+    }
+
+}
